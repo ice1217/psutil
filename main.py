30d70a94cb6d44da86f96f3f576414ed634f25cd
ice1217 <ivan.sitor1217@gmail.com>
2021-12-08 04:39:05 +0000

    remove api-based system, add insert to elasticsearch

--- main.py
+++ main.py
@@ -1,35 +1,33 @@
 # Importing the library
+import os
 import psutil
-import uvicorn
-import gunicorn
 from datetime import datetime
 import pytz
+from time import time, sleep
+from elasticsearch import Elasticsearch
 
-import ssl
 from psutil._common import bytes2human
-from fastapi import FastAPI
+server_name = os.environ['server_name']
+elastic_host = os.environ['elastic_host']
+elastic_port = os.environ['elastic_port']
+es = Elasticsearch(
+  [
+    {
+      'host': elastic_host,
+      'port': elastic_port
+    }
+  ]
+)
 
-app = FastAPI()
-
-try:
-    _create_unverified_https_context = ssl._create_unverified_context
-except AttributeError:
-# Legacy Python that doesn't verify HTTPS certificates by default
-    pass
-else:
-# Handle target environment that doesn't support HTTPS verification
-    ssl._create_default_https_context = _create_unverified_https_context
-
-@app.get("/")
-def read_root():
+def run_monitoring():
   timezone_WIB = pytz.timezone('Asia/Jakarta')
   current_time = datetime.now(timezone_WIB)
   # Calling psutil.cpu_precent() for 4 seconds
   cpu_percent = str(psutil.cpu_percent(1))
   cpu_percent_str = str(psutil.cpu_percent(1)) + '%'
   cpu_count = psutil.cpu_count()
-  print('CPU Count: ', cpu_count)
-  print('The CPU usage is: ', cpu_percent_str)
+  #print('CPU Count: ', cpu_count)
+  #print('The CPU usage is: ', cpu_percent_str)
 
   # Getting % usage of virtual_memory ( 3rd field)
   mem = psutil.virtual_memory()
@@ -45,13 +43,14 @@ def read_root():
   mem_used_str = bytes2human(mem[3])
   mem_free_str = bytes2human(mem[4])
 
-  print('memory total:', mem_total_str)
-  print('memory available:', mem_available_str)
-  print('memory percent:', mem_percent_str)
-  print('memory used:', mem_used_str)
-  print('memory free:', mem_free_str)
+  #print('memory total:', mem_total_str)
+  #print('memory available:', mem_available_str)
+  #print('memory percent:', mem_percent_str)
+  #print('memory used:', mem_used_str)
+  #print('memory free:', mem_free_str)
 
-  return {
+  doc = {
+    'server_name': server_name,
     'datetime': current_time,
     'cpu_count': cpu_count,
     'cpu_percent': cpu_percent,
@@ -60,9 +59,20 @@ def read_root():
     'mem_percent': mem_percent,
     'mem_used': mem_used,
     'mem_free': mem_free,
+    'cpu_percent_str': cpu_percent_str,
     'mem_total_str': mem_total_str,
     'mem_available_str': mem_available_str,
     'mem_percent_str': mem_percent_str,
     'mem_used_str': mem_used_str,
     'mem_free_str': mem_free_str
   }
+
+  es.index(index="tandai_monitoring", document=doc)
+  #print(res)
+  return doc
+
+
+while True:
+  doc = run_monitoring()
+  print(doc)
+  sleep(60 - time() % 60)
